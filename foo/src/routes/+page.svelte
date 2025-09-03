7c0d8bee597cfcd8a3a08efb7512e7692d4fd5da
KyanTG <kyan.gijtenbeek@hva.nl>
2025-09-03 16:08:19 +0200

    added data

---
+++ foo/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    let { data } = $props();
+
+    const members = data.members   
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#each members as member}
+    <a href="/{member.id}">{member.name}</a><br>
+    <img src="{member.avatar}"><br>
+{/each}
+
+<style>
+
+    img {
+        border-radius: 50px;
+        width: 200px;
+        height: 200px;
+    }
+
+</style>
+
