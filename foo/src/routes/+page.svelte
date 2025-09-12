b81ab80149526373b538148164b91bda9c045476
KyanTG <kyan.gijtenbeek@hva.nl>
2025-09-12 16:06:42 +0200

    implemented medias for tabbled, outlay and image,
    to do: titel en tekst

--- foo/src/routes/+page.svelte
+++ foo/src/routes/+page.svelte
@@ -32,7 +32,7 @@
     .outlay {
         display: flex;
         align-items: center;
-        justify-content: flex-start;
+        justify-content: flex;
         flex-direction: column;
         height: 200vh;
         font-family: 'CustomFont', sans-serif;
@@ -48,6 +48,13 @@
         border: 7.5px solid #7376B4;
         border-radius: 120px 60px 120px 60px;
         background-color: #1E2152;
+
+        @media (min-width: 834px) {
+            width: 28.125em;
+            height: 42em;
+            border: 11.25px solid #7376B4;
+            border-radius: 180px 90px 180px 90px;
+        }
     }
 
     .mycard-back {
@@ -58,6 +65,12 @@
         width: 9.531em;
         height: 9.531em;
         border-radius: 76px 38px 76px 38px;
+
+        @media (min-width: 834px) {
+            width: 14.25em;
+            height: 14.25em;
+            border-radius: 120px 55.5px 120px 55.5px;
+        }
     }
 
     div {
@@ -69,6 +82,12 @@
         border-radius: 76px 38px 76px 38px;
         background-color: #7376B4;
         margin-top: 48px;
+
+        @media (min-width: 834px) {
+            width: 14.25em;
+            height: 21.524em;
+            border-radius: 120px 55.5px 120px 55.5px;
+        }
     }
 
     h1 {
